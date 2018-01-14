9f719f58a4cc30de3ca53619dda5bc84634674e7
Brian Granger <brian.g@avviato.com>
2018-01-14 15:18:13 -0400

    Incluyendo hoja de estilos Incluyendo estilos inline y uso de clases css

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
+import './media.css';
 
 class Media extends Component {
 	render() {
+		const styles = {
+			container: {
+				//fontSize: 18, //Lo transforma a px
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid red'
+			}
+		}
 		return (
-			<div>
-				<div>
+			<div className="Media">
+				<div className="Media-cover">
 					<img
-						src=""
+						src="./images/covers/bitcoin.jpg"
 						alt=""
 						width={260}
-						height={160} 
+						height={160}
+						className="Media-image"
 					/>
-					<h3>Why do you need learn react?</h3>
-					<p>Carlos González</p>
+					<h3 className="Media-title">Why do you need learn react?</h3>
+					<p className="Media-author">Carlos González</p>
 				</div>
 			</div>
 		);
