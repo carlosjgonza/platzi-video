34845b44365dec96db091b014264ad7bbcae419a
Brian Granger <brian.g@avviato.com>
2018-01-14 14:40:49 -0400

    Cambiando el index.html al entorno de desarrollo Utilizando el reactDom para crear el primer elemento react

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hola Mundo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+
+//ReactDOM.render(que voy a renderizar, donde lo voy a renderizar);
+const holaMundo = <h1>Hola mundo</h1>;
+ReactDOM.render(holaMundo, app);
