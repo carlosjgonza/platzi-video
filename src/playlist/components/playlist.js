63557f61a0cc35d4e256d68606bc3664a2074dc9
Brian Granger <brian.g@avviato.com>
2018-01-18 23:48:08 -0400

    Utilizando PureComponents y componentes funcionales

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Media from './media.js';
 import './playlist.css';
 
-class Playlist extends Component {
-	render() {
-		const playlist = this.props.data.categories[0].playlist;
-		return (
-			<div className='Playlist'>
-				{
-					playlist.map((item) => {
-						return <Media key={item.id} {...item} />
-					})
-				}
-			</div>
-		)
-	}
+function Playlist(props) {
+	const playlist = props.data.categories[0].playlist;
+	return (
+		<div className='Playlist'>
+			{
+				playlist.map((item) => {
+					return <Media key={item.id} {...item} />
+				})
+			}
+		</div>
+	)
 }
 
 export default Playlist;
