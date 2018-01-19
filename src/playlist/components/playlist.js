96abb17ab303ec22f35eacd4585ef546f437d365
Brian Granger <brian.g@avviato.com>
2018-01-19 19:22:39 -0400

    Utilizando composición de elementos

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Media from './media.js';
+
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/fullscreen';
+
 import './playlist.css';
 
 function Playlist(props) {
 	const playlist = props.data.categories[0].playlist;
 	return (
 		<div className='Playlist'>
+			<Play color="red" size="30"/>
+			<Volume color="green" size="30"/>
+			<FullScreen color="blue" size="30" />
+			<Pause color="black" size="30" />
 			{
 				playlist.map((item) => {
 					return <Media key={item.id} {...item} />
