901e5f54e2a049ceb643cbea167aa9979e8d1da9
Brian Granger <brian.g@avviato.com>
2018-01-24 13:48:36 -0400

    Incluyendo botones de play y pause y agregando eventos para hacer toggle de los iconos

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video.js';
 import Title from '../components/title.js';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+	state = {
+		pause: true,
+	}
+	togglePlay = (event) => {
+		this.setState({
+			pause: !this.state.pause
+		})
+	}
 	render() {
 		return (
 			<VideoPlayerLayout>
 				<Title
 					title="Este es un titulo chido!!"
+				/>
+				<PlayPause 
+					pause={this.state.pause}
+					handleClick={this.togglePlay}
 				/>
 				<Video 
 					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
