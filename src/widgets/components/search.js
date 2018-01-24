1238894b6c16116ca813ae7c3328019b34ffb0b9
Brian Granger <brian.g@avviato.com>
2018-01-24 11:13:40 -0400

    Creando componentes con formularios

---
+++ src/widgets/components/search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './search.css';
+
+// function Search(props) {
+// 	return (
+
+// 	)
+// }
+
+const Search = () => (
+	<form className="Search" action="">
+		<input 	className="Search-input" 
+				type="text"
+				placeholder="Busca tu video favorito" />
+	</form>
+)
+
+export default Search;
