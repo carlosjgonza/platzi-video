ee6a25ea8be9e84bfc4afe49d75f61eeb345926f
Brian Granger <brian.g@avviato.com>
2018-01-24 10:58:25 -0400

    Utilizando el componentDidCatch

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -5,10 +5,11 @@ import Related from '../components/related';
 import Categories from '../../categories/components/categories';
 import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
+import HandleError from '../../errors/containers/handleError';
 
 class Home extends Component {
 	state = {
-		modalVisible: false,
+		modalVisible: false
 	}
 	handleCloseModal = (event) => {
 		this.setState({
@@ -22,23 +23,25 @@ class Home extends Component {
 	}
 	render() {
 		return (
-			<HomeLayout>
-				<Related />
-				<Categories 
-					categories={this.props.data.categories} 
-					handleOpenModal={this.handleOpenModal}
-					/>
-				{
-					this.state.modalVisible && 
-					<ModalContainer>
-						<Modal
-							handleClick={this.handleCloseModal}
-						>
-							<h1>Esto es un portal</h1>
-						</Modal>
-					</ModalContainer>
-				}
-			</HomeLayout>
+			<HandleError>
+				<HomeLayout>
+					<Related />
+					<Categories 
+						categories={this.props.data.categories} 
+						handleOpenModal={this.handleOpenModal}
+						/>
+					{
+						this.state.modalVisible && 
+						<ModalContainer>
+							<Modal
+								handleClick={this.handleCloseModal}
+							>
+								<h1>Esto es un portal</h1>
+							</Modal>
+						</ModalContainer>
+					}
+				</HomeLayout>
+			</HandleError>
 		)
 	}
 }
