2811e63707b1fd74ae7eefc028538e4bfec7eae5
Brian Granger <brian.g@avviato.com>
2018-01-25 09:57:15 -0400

    Incluyendo controles para manejar timer del videoPlayer

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -19,12 +19,17 @@ class Video extends Component {
 		}
 	}
 	render() {
+		const {
+			handleLoadedMetadata
+		} = this.props;
+		
 		return (
 			<div className="Video">
 				<video 
 					autoPlay={this.props.autoplay}
 					src={this.props.src}
 					ref={this.setRef}
+					onLoadedMetadata={handleLoadedMetadata}
 				/>
 			</div>
 		)
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -3,10 +3,13 @@ import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video.js';
 import Title from '../components/title.js';
 import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import Controls from '../components/video-player-controls';
 
 class VideoPlayer extends Component {
 	state = {
 		pause: true,
+		duration: 0
 	}
 	togglePlay = (event) => {
 		this.setState({
@@ -18,21 +21,33 @@ class VideoPlayer extends Component {
 			pause: (!this.props.autoplay)
 		})
 	}
+	handleLoadedMetadata = event => {
+		this.video = event.target;
+		this.setState({
+			duration: this.video.duration,
+		});
+	}
 	render() {
 		return (
 			<VideoPlayerLayout>
 				<Title
 					title="Este es un titulo chido!!"
 				/>
-				<PlayPause 
-					pause={this.state.pause}
-					handleClick={this.togglePlay}
-				/>
-				<Video 
+				<Video
+					handleLoadedMetadata={this.handleLoadedMetadata}
 					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
 					autoplay={this.props.autoplay}
 					pause={this.state.pause}
 				/>
+				<Controls>
+					<PlayPause 
+						pause={this.state.pause}
+						handleClick={this.togglePlay}
+					/>
+					<Timer
+						duration={this.state.duration}
+					/>
+				</Controls>
 			</VideoPlayerLayout>
 		)
 	}
