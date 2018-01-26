df94dd7d9402d2d4da76bb5b9d1f91e51aed73ca
Brian Granger <brian.g@avviato.com>
2018-01-25 21:25:24 -0400

    Incluyendo index.html with references of PROD

--- docs/src/pages/components/related.js
+++ docs/src/pages/components/related.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../../images/logo.png';
+import data from '../../api.json';
 import './related.css';
 
 function Related(props) {
 	return (
 		<div className="Related">
-			<img src={logo} width="250"/>
+			<img src={data.logo} width="250"/>
 		</div>
 	)
 }
